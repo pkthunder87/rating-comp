045170371a1cb001768fd97b01f986ac4df9a3b9
pkthunder87 <pkthunder87@gmail.com>
2024-12-14 22:43:28 -0800

    Feat: Add html to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Interactive Rating Component</div>;
+  return (
+    <div>
+      <p>
+        How did we do? Please let us know how we did with your support request.
+        All feedback is appreciated to help us improve our offering! 1 2 3 4 5
+        Submit You selected Thank you! We appreciate you taking the time to give
+        a rating. If you ever need more support, don’t hesitate to get in touch!
+      </p>
+    </div>
+  );
 }
 
 export default App;
