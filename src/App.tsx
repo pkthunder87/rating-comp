4aa3dfb5190fcdafaa924dd16c08a97303552b2b
pkthunder87 <pkthunder87@gmail.com>
2024-12-19 20:29:21 -0800

    Feat: Add Attribution.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import AppLayout from "./components/AppLayout";
 import "./sass/main.scss";
+import Attribution from "./components/Attribution";
 
 function App() {
   return (
-    <AppLayout>
-      <p className="text">
-        How did we do? Please let us know how we did with your support request.
-        All feedback is appreciated to help us improve our offering! 1 2 3 4 5
-        Submit You selected Thank you! We appreciate you taking the time to give
-        a rating. If you ever need more support, don’t hesitate to get in touch!
-      </p>
-    </AppLayout>
+    <div>
+      <AppLayout props="">
+        <p className="text">
+          How did we do? Please let us know how we did with your support
+          request. All feedback is appreciated to help us improve our offering!
+          1 2 3 4 5 Submit You selected Thank you! We appreciate you taking the
+          time to give a rating. If you ever need more support, don’t hesitate
+          to get in touch!
+        </p>
+      </AppLayout>
+      <Attribution />
+    </div>
   );
 }
 
---
+++ src/components/Attribution.tsx
@@ -0,0 +1,23 @@
+function Attribution() {
+  return (
+    <div className="attribution">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://https://github.com/pkthunder87/rating-comp"
+      >
+        PkThunder.
+      </a>
+    </div>
+  );
+}
+
+export default Attribution;
